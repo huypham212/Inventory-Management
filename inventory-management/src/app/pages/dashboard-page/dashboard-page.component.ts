79647c988d24123fa489bd6248444f0a23cc3957
huypham212 <thanhhuy2120@gmail.com>
2022-04-22 17:37:41 +0700

    finish function search

--- inventory-management/src/app/pages/dashboard-page/dashboard-page.component.ts
+++ inventory-management/src/app/pages/dashboard-page/dashboard-page.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import data from '../../data/data.json';
 
 @Component({
   selector: 'app-dashboard-page',
@@ -6,10 +7,47 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dashboard-page.component.scss']
 })
 export class DashboardPageComponent implements OnInit {
+  search = "";
+  suggestion: string[] = [];
+  result: object[] = [];
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor() {
   }
 
+  ngOnInit(): void {
+    data.forEach(element => {
+      this.suggestion.push(element.name_product);
+      //console.table(this.suggestion.sort());
+    })
+  }
+
+
+  onSearch = (search: string) => {
+    console.log(this.suggestion.filter((option) => option.toLowerCase().includes(search.toLowerCase())));
+  }
+
+  onKeyPress = (key: any, search: string) => {
+    this.result = [];
+    if (key.keyCode === 13) {
+      data.forEach(element => {
+        if (element['name_product'].includes(search)) {
+          this.result.push(element);
+        }
+      })
+
+      console.log(this.result);
+    }
+  }
+
+  onNameSort = () => {
+    return;
+  }
+
+  onDateSort = () => {
+    return;
+  }
+
+  onExport = () => {
+    console.log("Export!");
+  }
 }
