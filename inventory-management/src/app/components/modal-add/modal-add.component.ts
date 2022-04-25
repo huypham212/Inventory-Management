18c5be16c2f1c991d3fe4c7ff09bfa8d0cc70fd7
dt0801 <94697652+dt0810@users.noreply.github.com>
2022-04-25 13:58:14 +0700

    update modal-add

---
+++ inventory-management/src/app/components/modal-add/modal-add.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-modal-add',
+  templateUrl: './modal-add.component.html',
+  styleUrls: ['./modal-add.component.scss']
+})
+export class ModalAddComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
