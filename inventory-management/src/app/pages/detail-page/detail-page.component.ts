11acd785f7cd8136187caa04fd2f69455def6e81
nnam2501 <nnam2501@gmail.com>
2022-04-28 10:17:33 +0700

    update detail page

--- inventory-management/src/app/pages/detail-page/detail-page.component.ts
+++ inventory-management/src/app/pages/detail-page/detail-page.component.ts
@@ -1,7 +1,21 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { getDateString } from 'src/app/services';
+import { getProductById } from 'src/app/services';
 import dataa from '../../data/data.json';
+
+interface Data {
+  name: string;
+  quantity: number;
+  userCreate: string;
+  createDate: number;
+  userUpdate: string;
+  updateDate: string;
+  brandId: string;
+  categoryId: string;
+}
+
 @Component({
   selector: 'app-detail-page',
   templateUrl: './detail-page.component.html',
@@ -10,23 +24,36 @@ import dataa from '../../data/data.json';
 export class DetailPageComponent implements OnInit {
   datas: any;
   id: any;
-  constructor(private route: ActivatedRoute) { }
+  constructor(private route: ActivatedRoute, private http: HttpClient) {}
 
   ngOnInit() {
-    this.id = this.route.snapshot.params;
-    dataa.filter((item) => {
-      if (item.id === +this.route.snapshot.params['id']) {
-        this.datas = item;
-        console.log(item);
-      }
-    });
-    console.log(this.id);
-    console.log(this.route.snapshot.params['id']);
+    this.http
+      .get(getProductById + this.route.snapshot.params['id'], {
+        headers: {
+          Authorization: 'Bearer ' + localStorage.getItem('token'),
+        },
+      })
+      .subscribe((data) => {
+        console.log(data);
 
-    console.log(this.datas);
+        this.datas = data;
+        console.log(this.datas);
+      });
+    // console.log();
+    // this.id = this.route.snapshot.params;
+    // dataa.filter((item) => {
+    //   if (item.id === +this.route.snapshot.params['id']) {
+    //     this.datas = item;
+    //     console.log(item);
+    //   }
+    // });
+    console.log(this.route.snapshot.params['id']);
+    // console.log(this.datas.name);
+
+    // console.log(this.datas);
   }
 
-  onFilter = (search: number) => { };
+  onFilter = (search: number) => {};
 
   convertTimestampsToString = (value: number) => {
     return getDateString(value);
--- inventory-management/src/app/services/index.ts
+++ inventory-management/src/app/services/index.ts
@@ -1,48 +1,49 @@
 import { formatDate } from '@angular/common';
 
 export const getDateString = (datetime: number) => {
+  var datetimeString = formatDate(
+    new Date(datetime * 1000),
+    'dd/MM/yyyy hh:mm:ss a',
+    'en-US',
+    'GMT+07:00'
+  );
 
-    var datetimeString = formatDate(new Date(datetime * 1000), "dd/MM/yyyy hh:mm:ss a", "en-US", "GMT+07:00");
+  var arrDatetime = datetimeString.split(' ');
 
-    var arrDatetime = datetimeString.split(" ");
+  arrDatetime.forEach((element) => {
+    if (arrDatetime.indexOf(element) === 2) {
+      if (element == 'PM') {
+        arrDatetime[2] = 'CH';
+      } else {
+        arrDatetime[2] = 'SA';
+      }
+    }
+  });
 
-    arrDatetime.forEach(element => {
-        if (arrDatetime.indexOf(element) === 2) {
-            if (element == "PM") {
-                arrDatetime[2] = "CH";
-            }
-            else {
-                arrDatetime[2] = "SA";
-            }
-        }
-    })
-
-    return arrDatetime[0] + " " + arrDatetime[1] + " " + arrDatetime[2];
-
-}
-
+  return arrDatetime[0] + ' ' + arrDatetime[1] + ' ' + arrDatetime[2];
+};
 
 // Define API
-const baseURL = "https://localhost:5001/api/"
+const baseURL = 'http://192.168.2.212:6789/api/';
 
 //BrandsAPI
-export const getAllBrands = baseURL + "Brands"
-export const getBrandsByCatId = baseURL + "Brands/"
-export const getBrandsByBrandId = baseURL + "Brands/name/"
+export const getAllBrands = baseURL + 'Brands';
+export const getBrandsByCatId = baseURL + 'Brands/';
+export const getBrandsByBrandId = baseURL + 'Brands/name/';
 
 //CategoriesAPI
-export const getAllCategories = baseURL + "Categories"
-export const getCategoriesByCatId = baseURL + "Categories/name/"
-export const getCategoriesByBrandId = baseURL + "Categories/"
+export const getAllCategories = baseURL + 'Categories';
+export const getCategoriesByCatId = baseURL + 'Categories/name/';
+export const getCategoriesByBrandId = baseURL + 'Categories/';
 
 //ProductAPI
-export const getAllProduct = baseURL + "Products"
-export const getProductById = baseURL + "Products/"
-export const postProduct = baseURL + "Products"
-export const putProduct = baseURL + "Products/"
-export const putDelProduct = baseURL + "Products/delete/"
-export const deleteProduct = baseURL + "Products/"
+export const getAllProduct = baseURL + 'Products';
+export const getProductById = baseURL + 'Products/';
+export const postProduct = baseURL + 'Products';
+export const putProduct = baseURL + 'Products/';
+export const putDelProduct = baseURL + 'Products/delete/';
+export const deleteProduct = baseURL + 'Products/';
 
 //LoginAPI
-export const postLogin = baseURL + "Users/login"
-export const getUserName = baseURL + "Users/name"
+export const postLogin = baseURL + 'Users/login';
+export const getUserName = baseURL + 'Users/name';
